26bf46c12fcccd38d5020473e9149482aaacb2f0
SongZi <greensun.h@gmail.com>
2023-10-23 21:00:14 +0800

    first app test.

--- my-vue-projects/my-first-app/src/App.vue
+++ my-vue-projects/my-first-app/src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>父组件中接收到的数据：{{ childData }}</h1>
+    <!-- 可以在子组件中规定插槽在哪个位置进行呈现 -->
+    <HelloWorld 
+      msg="Welcome to Your Vue.js App"
+      :count="parentCount"  
+      @child-count-change="handler"
+    >
+      这是默认内容1
+      <template v-slot:footer="{ childCount }">
+        这是第一个footer{{ childCount }}
+      </template>
+    </HelloWorld>
+    <HelloWorld>
+      这是默认内容2
+    </HelloWorld>
+    <HelloWorld>
+      这是默认内容3 
+    </HelloWorld>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
@@ -12,6 +30,19 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      parentCount: 200, // 父组件中的响应式数据，通过props传递给子组件
+      childData: 0
+    }
+  },
+  methods: {
+    // 子组件自己定义了一个方法叫做child-count-change，由父组件做一个监听，
+    // 只要子组件触发了这个事件（通过$emit()），父组件就会执行这个方法
+    handler (childCount) {
+      this.childData = childCount
+    }
   }
 }
 </script>
--- my-vue-projects/my-first-app/src/components/HelloWorld.vue
+++ my-vue-projects/my-first-app/src/components/HelloWorld.vue
@@ -1,46 +1,49 @@
 <template>
   <div class="hello">
+    <!-- 默认插槽 -->
+    <slot>基础的默认内容</slot>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>props中的count是：{{ count }}</p>
+    <button @click="handler">按钮</button>
+    <!-- 具名插槽 -->
+    <slot name="footer" :childCount="childCount">footer的默认内容</slot>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  // 组件通信
+  // 1.父传子：props 进行处理
+  // 2.子传父：自定义事件 $emit()
+  props: {  
+    msg: String,
+    count: {
+      type: [String, Number],
+      // default: 100,  // default：默认值 
+      required: true  // required：必须使用父组件传递的数据 
+    }
+  },
+  data () {
+    return {
+      childCount: 1
+    }
+  },
+  methods: {
+    handler () {
+      this.childCount++
+      // 子传父：$emit
+      this.$emit('child-count-change', this.childCount)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  background-color: antiquewhite;
+}
 h3 {
   margin: 40px 0 0;
 }
