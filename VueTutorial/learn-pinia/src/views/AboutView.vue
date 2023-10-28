64d66ac562da146e4654188ce411c0b45832bc14
SongZi <greensun.h@gmail.com>
2023-10-28 17:49:31 +0800

    pinia

---
+++ VueTutorial/learn-pinia/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useCounterStore } from '../stores/counter'
+const countStore = useCounterStore()
+</script>
+
+<template>
+  <div class="about">
+    <h1>count: {{ countStore.count }}</h1>
+    <h2>2*count：{{ countStore.doubleCount }}</h2>
+    <button @click="countStore.increment">+1</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
